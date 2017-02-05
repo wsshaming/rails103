749751903e4d08a327b564ed1835ee60197d65d7
明沙 <mingsha@mingshadeMacBook-Pro.local>
2017-02-05 12:41:14 +0800

    implement post create function

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170204061733) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.text     "centent"
+    t.text     "content"
     t.integer  "group_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
